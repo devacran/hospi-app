7d32a906596062ff486996dcb2ea5918e1b44ea3
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2021-02-13 13:20:54 -0600

    initialize project

---
+++ hospi_app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Login } from "./pages/Login/Login";
+const App = () => {
+  return <Login />;
+};
+
+export default App;
