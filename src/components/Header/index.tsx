c2e74580e3a83b7ac49a92913d7300d1de349e26
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2021-02-27 12:09:59 -0600

    header routes add

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,16 @@ import { useLocation } from "react-router-dom";
 
 export const Header = () => {
   const location = useLocation();
-  console.log(location);
+  const patientId = location.pathname.split("/")[3];
+  const childRoute = location.pathname.split("/")[4];
   const isRoute = {
     patients: location.pathname === "/app/patients",
-    patient: location.pathname === "/app/patient/:id",
-    vitalSigns: location.pathname.split("/")[3] === "vital-signs",
+    patientDashboard: patientId && !childRoute,
+    vitalSigns: childRoute === "vital-signs",
+    billAccount: childRoute === "bill-account",
+    prescriptions: childRoute === "prescriptions",
   };
-
+  console.log(isRoute);
   return (
     <div className="m-3">
       <h1 className="h1">
@@ -17,6 +20,12 @@ export const Header = () => {
           ? "Pacientes"
           : isRoute.vitalSigns
           ? "Signos Vitales"
+          : isRoute.patientDashboard
+          ? "Informacion del Paciente"
+          : isRoute.billAccount
+          ? "Estado de Cuenta"
+          : isRoute.prescriptions
+          ? "Prescripciones"
           : ""}
       </h1>
     </div>
