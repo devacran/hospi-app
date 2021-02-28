65d6f5b39ce42ecd7c615ec9b4da03a9646417a2
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2021-02-28 10:50:25 -0600

    add modal to add mecinides

--- src/pages/Prescriptions/index.tsx
+++ src/pages/Prescriptions/index.tsx
@@ -1,30 +1,112 @@
-import { TableBuilder, rowCreator } from "../../components/TableBuilder";
-import { InputGroup, Button, FormControl } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  TableBuilder,
+  rowCreator,
+  RowCreator,
+} from "../../components/TableBuilder";
 import { SubHeader } from "../../components/SubHeader";
-
+import { Button, Modal } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
+import Select from "react-select/async";
+import { RemoveCircleOutlineOutlined } from "@material-ui/icons";
 import classes from "./styles.module.scss";
 
+type MedicineType = {
+  id: number;
+  compound: string;
+  unitPrice: number;
+  concentration: string;
+};
 export const Prescriptions = () => {
-  const rows = [
-    rowCreator(["1523", "12312312", "12313", "123123", "123123"]),
-    rowCreator(["1523", "12312312", "12313", "123123", "123123"]),
-  ];
+  const [modal, setModal] = useState(false);
+  const [rowElements, setRowElements] = useState<RowCreator[]>([]);
 
+  const [selectedItems, setSelectedItems] = useState<MedicineType[]>([]);
+  const options = [
+    { id: 1, compound: "Paracetamol", unitPrice: 123, concentration: "23/g" },
+    { id: 2, compound: "Otra medicina", unitPrice: 123, concentration: "23/g" },
+    { id: 3, compound: "Otra mas", unitPrice: 123, concentration: "23/g" },
+  ];
+  const handleChange = (item: any) => {
+    setSelectedItems([...selectedItems, item]);
+  };
+  const handleRemoveItem = (item: any) => {
+    const i = selectedItems.findIndex((item) => item.id === 1);
+    const newSelectedItems = selectedItems.splice(i + 1, 1);
+    console.log(i, newSelectedItems);
+    setSelectedItems(newSelectedItems);
+  };
+  const handleAddItems = () => {
+    setModal(false);
+    const rows = selectedItems.map((i) =>
+      rowCreator([
+        i.id,
+        i.compound + " " + i.concentration,
+        "---",
+        "---",
+        "---",
+      ])
+    );
+    setRowElements([...rowElements, ...rows]);
+    setSelectedItems([]);
+  };
+  console.log(selectedItems);
   return (
     <>
       <SubHeader>
         <div></div>
-        <Button>Añadir Medicamento</Button>
+        <Button onClick={() => setModal(true)}>Añadir Registro</Button>
       </SubHeader>
       <TableBuilder
-        rowsData={rows}
         hasActions={true}
+        rowsData={rowElements}
         cols={["Clave", "Medicamento", "Dosis", "Via Admin", "Frecuencia"]}
         onDelete={() => {}}
         onAdd={() => {}}
         onCancel={() => {}}
         onConfirm={() => {}}
       />
+      <Modal
+        show={modal}
+        onHide={() => setModal(false)}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Administracion de Medicamentos</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Select
+            onChange={handleChange}
+            placeholder={"Ingresa el nombre del medicamento"}
+            loadOptions={() => Promise.resolve(options)}
+          />
+          <ListGroup>
+            {selectedItems.map((i) => {
+              return (
+                <ListGroup.Item>
+                  <span>
+                    <Button onClick={() => handleRemoveItem(i)}>
+                      <RemoveCircleOutlineOutlined />
+                    </Button>
+                  </span>
+                  {i.compound}
+                </ListGroup.Item>
+              );
+            })}
+          </ListGroup>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setModal(false)}>
+            Close
+          </Button>
+          <Button onClick={handleAddItems} variant="primary">
+            Save changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
--- src/pages/VitalSigns/index.tsx
+++ src/pages/VitalSigns/index.tsx
@@ -1,46 +1,24 @@
-import React, { useState } from "react";
 import { TableBuilder, rowCreator } from "../../components/TableBuilder";
+import { InputGroup, Button, FormControl } from "react-bootstrap";
 import { SubHeader } from "../../components/SubHeader";
-import { Button, Modal } from "react-bootstrap";
-import { ListGroup } from "react-bootstrap";
-import Select from "react-select/async";
-import { RemoveCircleOutlineOutlined } from "@material-ui/icons";
 
 import classes from "./styles.module.scss";
-export const VitalSigns = () => {
-  const [modal, setModal] = useState(false);
-  const [selectedItems, setSelectedItems] = useState<
-    { value: string; label: string }[]
-  >([]);
 
+export const VitalSigns = () => {
   const rows = [
-    rowCreator(["1523", "12312312", "12313", "123123"]),
-    rowCreator(["1523", "12312312", "12313", "123123"]),
+    rowCreator(["1523", "12312312", "12313", "123123", "123123"]),
+    rowCreator(["1523", "12312312", "12313", "123123", "123123"]),
   ];
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
-  const handleChange = (item: any) => {
-    setSelectedItems([...selectedItems, item]);
-  };
-  const handleRemoveItem = (item: any) => {
-    const i = selectedItems.findIndex((item) => item.label === "Chocolate");
-    const newSelectedItems = selectedItems.splice(i + 1, 1);
-    console.log(i, newSelectedItems);
-    setSelectedItems(newSelectedItems);
-  };
-  console.log(selectedItems);
+
   return (
     <>
       <SubHeader>
         <div></div>
-        <Button onClick={() => setModal(true)}>Añadir Registro</Button>
+        <Button>Añadir Registro</Button>
       </SubHeader>
       <TableBuilder
-        hasActions={true}
         rowsData={rows}
+        hasActions={true}
         cols={[
           "Temp",
           "Nivel de Glucosa",
@@ -52,45 +30,6 @@ export const VitalSigns = () => {
         onCancel={() => {}}
         onConfirm={() => {}}
       />
-      <Modal
-        show={modal}
-        onHide={() => setModal(false)}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Administracion de Medicamentos</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <Select
-            onChange={handleChange}
-            placeholder={"Ingresa el nombre del medicamento"}
-            loadOptions={() => Promise.resolve(options)}
-          />
-          <ListGroup>
-            {selectedItems.map((i) => {
-              return (
-                <ListGroup.Item>
-                  <span>
-                    <Button onClick={() => handleRemoveItem(i)}>
-                      <RemoveCircleOutlineOutlined />
-                    </Button>
-                  </span>
-                  {i.label}
-                </ListGroup.Item>
-              );
-            })}
-          </ListGroup>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setModal(false)}>
-            Close
-          </Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal>
     </>
   );
 };
